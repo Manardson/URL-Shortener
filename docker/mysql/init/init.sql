d1fcef86fcfbd0275b3e9f6752e3de4598783577
Sebastian C <explorehumanity@gmail.com>
2025-02-25 11:47:05 +0000

    Update composer.lock, init.sql, and site.conf

--- docker/mysql/init/init.sql
+++ docker/mysql/init/init.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE IF NOT EXISTS `lesgo`;
-GRANT ALL ON `lesgo`.* TO 'docker'@'%';
+CREATE DATABASE IF NOT EXISTS `url`;
+GRANT ALL ON `url`.* TO 'docker'@'%';
 ALTER USER 'docker'@'localhost' IDENTIFIED WITH mysql_native_password BY 'secret';
 ALTER USER 'docker'@'%' IDENTIFIED WITH mysql_native_password BY 'secret';
